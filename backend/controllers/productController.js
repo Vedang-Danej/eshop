bfcc5152e6fa9f66e86e4b23198ab3692c47fccd
Vedang Danej <vedangdanej34@gmail.com>
2022-10-27 13:58:49 +0530

    fixing bugs

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 export const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 10;
+  const pageSize = 100;
   const page = Number(req.query.pageNumber) || 1;
   const keyword = req.query.keyword
     ? {
