b63090ec3e1e936014a5a08511f8d43fbc14b6bd
Vedang Danej <vedangdanej34@gmail.com>
2022-11-05 13:41:47 +0530

    CORS allowed

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,12 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
